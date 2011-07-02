d3a208349bab61e4bbdaf9b5af4b0de921707851
Michael Davis <m.r.davis@me.com>
2011-07-02 07:58:31 +0000

    Delete the pointers to the text files that were deleted

--- setup.py
+++ setup.py
@@ -12,25 +12,7 @@ def read(*rnames):
 version = open(os.path.join("cmrs", "academic", "version.txt")).read().strip()
 
 long_description = (
-    read('README.txt')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('CHANGES.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('cmrs', 'academic', 'README.txt')
-    + '\n' +
-    'Contributors\n'
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    + '\n' +
-    'Download\n'
-    '********\n')
+    read('cmrs', 'academic', 'README.txt'))
 
 tests_require = ['zope.testing']
 
