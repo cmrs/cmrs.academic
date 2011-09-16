4715ca407701336b6e54a55f1890d25118c48c19
Michael Davis <m.r.davis@me.com>
2011-09-16 16:03:22 +0000

    Make the section images work for academic folders

--- cmrs/academic/content/academicfolder.py
+++ cmrs/academic/content/academicfolder.py
@@ -26,4 +26,8 @@ class AcademicFolder(ATFolder):
     def canSetConstrainTypes(self):
         return False
 
+    security.declarePublic('getSectionFolder')
+    def getSectionFolder(self):
+        return self
+
 registerType(AcademicFolder, PROJECTNAME)
